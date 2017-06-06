0c8dccbee29779eb1380e64ab1e186b16ecaf64f
Chris S. Castaneda-Barajas <chrisscastaneda@gmail.com>
2017-06-05 21:06:46 -0700

    Consolidated shiny app into one file.

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 source("merge.R")
 
-function(input, output) {
+server <- function(input, output) {
   output$contents <- renderTable({
 
     addressFile <- input$addressFile
@@ -37,3 +37,30 @@ function(input, output) {
     #      quote=input$quote)
   })
 }
+
+ui <- fluidPage(
+  titlePanel("Merge Address and Polling Location Files"),
+  sidebarLayout(
+    sidebarPanel(
+      tags$h3("Upload your files here"),
+      tags$p(HTML("Make sure your address file is a <b>.csv</b> and contains the following column headers in the following order: <br><i>Street, Apt, City, State, Zip, Precinct ID</i>")),
+      fileInput('addressFile', 'Upload Address File:',
+                accept=c('text/csv', 
+                 'text/comma-separated-values,text/plain', 
+                 '.csv')),
+
+      tags$p(HTML("Make sure your polling location file is a <b>.csv</b> and contains the following column headers in the following order: <br><i>Street, City, State/ZIP, Country, Precinct</i>")),
+      fileInput('pollingFile', 'Upload Polling Location File:',
+                accept=c('text/csv', 
+                 'text/comma-separated-values,text/plain', 
+                 '.csv')),
+
+      tags$hr()
+    ),
+    mainPanel(
+      tableOutput('contents')
+    )
+  )
+)
+
+shinyApp(ui = ui, server = server)
